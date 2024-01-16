7fc3ab3e4d5b7ba1beba323a684854e93d199907
wiraizkandar <wiraizkandar1@gmail.com>
2024-01-16 23:08:49 +1100

    add gitignore

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!2")
+        return c.SendString("Hello, World!2344")
     })
 
     app.Listen(":3000")
