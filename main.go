e01f72b2c2acce2354507579b7c8c2f0788ee25c
wiraizkandar <wiraizkandar1@gmail.com>
2024-01-19 19:12:57 +1100

    try bycrypt

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!2344")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
 
-    app.Listen(":3000")
+		password := "123"
+		dbHash := "$2a$12$lZRJf/dTrnSxJ.itxDaprePEm8a3TVwoQh0T3px.7uoIyadoAO0EG"
+
+		match := CheckPasswordHash(password, dbHash)
+
+		fmt.Println("Password:", password)
+		fmt.Println("Hash:    ", dbHash)
+		fmt.Println("Match:   ", match)
+
+		return c.SendString("Hello, World!2asasa22344")
+
+	})
+
+	app.Listen(":3000")
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
